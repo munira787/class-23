67e876a2befa32fa996c08c027358724647e5653
munira787 <80512710+munira787@users.noreply.github.com>
2021-04-20 14:56:58 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box{
+    constructor(x, y, w, h){
+        var box_options ={
+            isStatic: false,
+            restitution: 0.4
+        }
+    
+        this.body = Bodies.rectangle(x, y, w, h,box_options);
+        this.w = w;
+        this.h = h;
+        World.add(world,this.body);
+    }
+    display(){
+        rectMode(CENTER);
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
+}
---
+++ ground.js
@@ -0,0 +1,16 @@
+class Ground{
+    constructor(x, y, w, h){
+        var ground_options ={
+            isStatic: true
+        }
+    
+        this.body = Bodies.rectangle(x, y, w, h,ground_options);
+        this.w = w;
+        this.h = h;
+        World.add(world,this.body);
+    }
+    display(){
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.w, this.h);
+    }
+}
